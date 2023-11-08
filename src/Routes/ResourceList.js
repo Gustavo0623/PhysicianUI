8039b48292cb2071c316925b34f0bc14e769baab
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-11-08 18:01:45 -0500

    Create resourcelist route

---
+++ src/Routes/ResourceList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Resource List
+
+export const ResourceList = () => {
+
+    return(
+        // Form
+        <div className="newResource">
+            <h1 className="header">Add New Com Resource</h1>
+        </div>
+    )
+}
+
+export default ResourceList
